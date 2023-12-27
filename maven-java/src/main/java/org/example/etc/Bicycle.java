63640876e476b526111b94906f776c8324d94294
nasif md tanjim <nasi.tanjim@metafour.com>
2023-12-27 21:01:42 +0600

    Bicycle

---
+++ maven-java/src/main/java/org/example/etc/Bicycle.java
@@ -0,0 +1,11 @@
+package org.example.etc;
+
+interface Bicycle {
+    void changeCadence(int value);
+
+    void changeGear(int value);
+
+    void speedUp(int increment);
+
+    void applyBrakes(int decrement);
+}
